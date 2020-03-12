1c79b4c332cd1b8a9e10e7dbb57c09240b01e81d
Jose Checa Claudel <jcheca@uco.es>
2020-03-12 23:04:34 +0100

    Update 12.03.2020

--- posadasiot/pointsroute.py
+++ posadasiot/pointsroute.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 # importing the requests library 
+from datetime import datetime, timedelta
 import requests, json
 import geopy.distance
 
@@ -16,8 +17,8 @@ HEADER = {
     "Fiware-ServicePath": "/rabanales"
 }
 PARAMS = {
-    "fromDate": "2020-03-10T06:00:00.000",
-    "toDate": "2020-03-10T13:00:00.000"
+    "fromDate": "2020-03-12T06:00:00.000",
+    "toDate": "2020-03-12T13:00:00.000"
 }
 
 URL1 = "https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json"
@@ -27,6 +28,7 @@ r = requests.get(url = URL, params = PARAMS, headers = HEADER)
   
 # extracting data in json format 
 data = r.json() 
+#print(data["data"]["attributes"][3]["values"])
 
 features = []
  
@@ -36,7 +38,25 @@ localiza = ""
 salida = ""
 
 while i < len(data["data"]["attributes"][0]["values"])-1:
-      localiza = str(data["data"]["attributes"][0]["values"][i])+", "+str(data["data"]["attributes"][1]["values"][i])+",250"
+
+      # datos analizador
+      battery_level = data["data"]["attributes"][0]["values"][i]
+      dl_counter = data["data"]["attributes"][1]["values"][i]
+      gps_quality = data["data"]["attributes"][2]["values"][i]
+      hdop = data["data"]["attributes"][3]["values"][i]
+      lati_hemisphere = data["data"]["attributes"][4]["values"][i]
+      latitude = data["data"]["attributes"][5]["values"][i]
+      lattitude_dm = data["data"]["attributes"][6]["values"][i]
+      long_hemisphere = data["data"]["attributes"][7]["values"][i]
+      longitude = data["data"]["attributes"][8]["values"][i]
+      longitude_dm = data["data"]["attributes"][9]["values"][i]
+      rssi_dl = data["data"]["attributes"][11]["values"][i]
+      sats = data["data"]["attributes"][12]["values"][i]
+      snr_dl = data["data"]["attributes"][13]["values"][i]
+      temperature = data["data"]["attributes"][14]["values"][i]
+      ul_counter = data["data"]["attributes"][15]["values"][i]
+      localiza = str(data["data"]["attributes"][5]["values"][i])+", "+str(data["data"]["attributes"][8]["values"][i])+",250"
+
       salida3 = data["data"]["index"][i]
       PARAMS1 = {
           "prox": localiza,
@@ -55,7 +75,6 @@ while i < len(data["data"]["attributes"][0]["values"])-1:
 
       try:
          salida1 = json.dumps(data1["Response"]["View"][0]["Result"][0]["Location"]["Address"]["Street"], ensure_ascii=False)
-         #salida1 = data1["Response"]["View"][0]["Result"][0]["Location"]["Address"]["Street"]
       except:
          salida1 = ""
 
@@ -64,7 +83,7 @@ while i < len(data["data"]["attributes"][0]["values"])-1:
       except:
          salida2 = ""
  
-      print "{} - {} - {}".format(salida1, salida2, salida3)
+      print "{},{},{},{},{},{},{},{},{},{},{},{},{}".format(battery_level,gps_quality,lati_hemisphere,latitude,long_hemisphere,longitude,rssi_dl,sats,snr_dl, temperature, salida1, salida2, salida3)
 
       i = i + 1
       localiza = []
