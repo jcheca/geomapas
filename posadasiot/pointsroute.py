36a23930716dac94473c8f7472edadd7c11c387d
Jose Checa Claudel <jcheca@uco.es>
2020-06-06 02:18:32 +0200

    Update 06 June

--- posadasiot/pointsroute.py
+++ posadasiot/pointsroute.py
@@ -17,8 +17,8 @@ HEADER = {
     "Fiware-ServicePath": "/rabanales"
 }
 PARAMS = {
-    "fromDate": "2020-06-01T03:00:00.000",
-    "toDate": "2020-06-01T14:00:00.000"
+    "fromDate": "2020-06-05T03:00:00.000",
+    "toDate": "2020-06-05T14:00:00.000"
 }
 
 URL1 = "https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json"
